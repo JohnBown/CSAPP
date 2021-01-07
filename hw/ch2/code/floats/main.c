4b07aa206fb8e0f9d742bb52044d4442e982e341
johnbown <yuanchaoradom@gmail.com>
2021-01-07 19:31:21 +0800

    modified main.c

--- hw/ch2/code/floats/main.c
+++ hw/ch2/code/floats/main.c
@@ -6,6 +6,10 @@
 
 #include "float-negate.h"
 #include "float-absval.h"
+#include "float-twice.h"
+#include "float-half.h"
+#include "float-f2i.h"
+#include "float-i2f.h"
 
 unsigned f2u(float f)
 {
@@ -34,15 +38,34 @@ int main(int argc, char *argv[])
         float f = u2f(r);
         printf("f:\t0x%.8X\t%.50f\n", f2u(f), f);
 
+        float fmul2 = f * 2.0;
+        printf("fmul2:\t0x%.8X\t%.50f\n", f2u(fmul2), fmul2);
+
+        float fdiv2 = f * 0.5;
+        printf("fdiv2:\t0x%.8X\t%.50f\n", f2u(fdiv2), fdiv2);
+
+        int f2i = (int)f;
+        printf("f2i:\t0x%.8X\t%d\n", f2i, f2i);
+
+        float i2f = (float)(int)r;
+        printf("i2f:\t0x%.8X\t%.50f\n", f2u(i2f), i2f);
+
         if (isnan(f))
         {
             assert(float_negate(r) == r);
             assert(float_absval(r) == r);
+            assert(float_twice(r) == r);
+            assert(float_half(r) == r);
+            assert(float_f2i(r) == f2i);
         }
         else
         {
             assert(u2f(float_negate(r)) == -f);
             assert(u2f(float_absval(r)) == fabsf(f));
+            assert(u2f(float_twice(r)) == fmul2);
+            assert(u2f(float_half(r)) == fdiv2);
+            assert(float_f2i(r) == f2i);
+            assert(u2f(float_i2f(r)) == i2f);
         }
 
         printf("\n");
